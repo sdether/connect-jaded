1424d1f985786738cfb143f0ed7b1e73de10853b
Arne Claassen <arne@claassen.net>
2013-10-28 22:51:25 -0700

    data callback, cache key support and data staleness checking

--- README.md
+++ README.md
@@ -1,14 +1,96 @@
-jaded
-=====
+jaded 0.1a
+=========
 
-Static html generator connect middleware for infrequently changing jade templates
+jaded (pronounced jade-dee) provides connect middleware to generate static html on demand for infrequently
+changing jade templates/data. When the template and render data changes infrequently, jaded provides the benefit
+of responding with a file send rather than a template execute and it will properly handle handle cache headers to
+respond with a `304 NOT MODIFIED` rather than a `200 OK`.
 
-This to in no way shape or form ready for use.
+This code is alpha quality. Check TODO below to see what is missing before it should be considered for anything
+but experimental use.
 
-So what's left before it's even worth trying?
+Features
+--------
+* Generate static html on demand from jade templates
+* Match request path based on various patterns
+* Provide data to jade templates (should be used for static configuration)
+* Invalidate html based on template and dependent template changes
+* Invalidate html based on data changes
+* Generate different html caches based on request level information
 
-* ~~Get staleness checking based on mtime~~
-* Get jade modifications for tracking template dependencies accepted upstream
-* Add a callback for providing template locals
-* Add template data hashing to determine staleness based on data changes
-* remove all the local testing junk (i.e. node_modules, etc.)
+Usage
+-----
+
+```javascript
+app.use(jaded.middleware({
+  templatepath: path.join(__dirname, 'templates'),
+}));
+```
+
+Options
+-------
+* `templatepath` **required**
+  * path to search for templates
+* `cachepath`
+  * path where generated output is stored
+  * default: `templatepath` */.cache*
+* `ext`
+  * extension to identify a path as a file rather than a directory
+  * default: *.html*
+* `notrailingslash`
+  * is a path ending without a / the canonical path
+  * default: *false*
+* `ignore`
+  * list of path prefixes to ignore for jaded processing
+* `wildcards`
+  * consider a trailing underscore as a wildcard for sub-path matches
+  * default: *false*
+* `getData`
+  * a function of the form
+
+    `function(req, res, candidate, callback)`
+
+    where:
+
+    * req - request
+    * res - response
+    * candidate - the matched template fragment
+    * callback - `function(err, data, key)`
+
+Why: Use case
+---------
+In frameworks, such as Angular, the HTML is usually static with all dynamic content being loaded via services.
+In order to retain the syntax of jade instead of HTML, the composition abilities of `extends` and `includes` and
+site or path specific configuration data that is shared between templates, jaded hooks in connect to intercept requests
+and, as appropriate, generate and serve static html. It is further possible to generate different static html based on
+request specific information (such as html for logged-in vs. anonymous users).
+
+Path Pattern matching
+---------------------
+jaded will match a path as either an html file or index file for a directory.
+
+In the case of a filepath, no wildcards will considered, i.e. a request for `/foo/bar.html` will look only for
+`foo/bar.jade`.
+
+Directory paths first canonalize path to either requiring a trailing slash or not. If `options.notrailingslash` is set,
+the canonical uri for `/foo/bar/` is `foo/bar` and jaded will redirect as appropriate, if not set the opposite is the
+canonical uri.
+
+Once on a canonical path, `foo/bar` will consider the following templates in the shown order:
+* `/foo/bar`
+* `/foo/bar/index`
+* `/foo/bar_`
+* `/foo_`
+* `/_`
+The first one to exist is used as the matching template and checked against an existing .html file to determine whether
+the output needs to be generated or served from disk.
+
+TODO
+----
+- ~~Staleness checking on templates and dependencies based on mtime~~
+- ~~Matching paths based on index and wildcard patterns~~
+- ~~Redirect behavior to canonalize either /foo/ or /foo~~
+- ~~Get a callback for providing template locals && optional cache key~~
+- ~~Add template data hashing to determine staleness for callback provided locals~~
+- tests
+- [Get jade modifications for tracking template dependencies accepted upstream](https://github.com/visionmedia/jade/pull/1252)
--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function initialize(options) {
   }
   _options.ext = _options.ext || '.html';
 }
+
 function middleware(options) {
   if(options) {
     initialize(options);
@@ -70,6 +71,53 @@ function middleware(options) {
     }
     console.log('files: ' + JSON.stringify(files));
     console.log('redirect: ' + redirect);
+
+    function getCandidate(candidates, callback) {
+      if(!candidates || candidates.length == 0) {
+        return callback();
+      }
+      var candidate = _.first(candidates);
+      var jadePath = path.join(_options.templatepath, candidate + '.jade');
+      return fs.stat(jadePath, function(err, jadeStats) {
+        if(err) {
+          if(!err.code || err.code != 'ENOENT') {
+            return callback(err);
+          }
+          return getCandidate(_.rest(candidates), callback);
+        }
+        getData(candidate, function(err, data, key) {
+          var htmlPath = key
+            ? path.join(_options.cachepath, key, candidate + _options.ext)
+            : path.join(_options.cachepath, candidate + _options.ext);
+          return fs.stat(htmlPath, function(err, htmlStats) {
+            if(err) {
+              if(!err.code || err.code != 'ENOENT') {
+                return callback(err);
+              }
+            }
+            return callback(null, {
+              html: {
+                path: htmlPath,
+                stats: htmlStats
+              },
+              jade: {
+                path: jadePath,
+                stats: jadeStats
+              },
+              data: data || {}
+            });
+          });
+        });
+      });
+    }
+
+    function getData(candidate, callback) {
+      if(!_options.getData) {
+        return callback(null, {});
+      }
+      return _options.getData(req, res, candidate, callback);
+    }
+
     return getCandidate(files, function(err, candidate) {
       if(err) {
         return next(err);
@@ -112,10 +160,11 @@ function middleware(options) {
           try {
             var info = jade.resolve(jadeStr, options);
             info.files = flatten(info.dependencies);
-            _infoCache[candidate.jade.path] = info;
+            info.data = candidate.data;
+            _infoCache[candidate.html.path] = info;
             console.log(JSON.stringify(info.files, null, 2));
             console.log('gen html');
-            var html = info.fn({title: 'hi there'});
+            var html = info.fn(info.data);
             var htmlDir = path.dirname(candidate.html.path);
             return mkdirp(htmlDir, 0755, function(err) {
               if(err) {
@@ -142,9 +191,13 @@ function checkCache(candidate, callback) {
     console.log("no html");
     return callback(null, true);
   }
-  var info = _infoCache[candidate.jade.path];
+  var info = _infoCache[candidate.html.path];
   if(!info) {
     console.log("no jade info cached yet");
+    return callback(null, true);
+  }
+  if(!objectEquals(candidate.data,info.data)) {
+    console.log("data changed");
     return callback(null, true);
   }
   var htmlTime = candidate.html.stats.mtime;
@@ -169,38 +222,27 @@ function checkCache(candidate, callback) {
   return iterator(info.files);
 }
 
-function getCandidate(candidates, callback) {
-  if(!candidates || candidates.length == 0) {
-    return callback();
-  }
-  var candidate = _.first(candidates);
-  var htmlPath = path.join(_options.cachepath, candidate + _options.ext);
-  var jadePath = path.join(_options.templatepath, candidate + '.jade');
-  return fs.stat(jadePath, function(err, jadeStats) {
-    if(err) {
-      if(!err.code || err.code != 'ENOENT') {
-        return callback(err);
-      }
-      return getCandidate(_.rest(candidates), callback);
+function objectEquals(x, y) {
+  if (x instanceof Function) {
+    if (y instanceof Function) {
+      return x.toString() === y.toString();
     }
-    return fs.stat(htmlPath, function(err, htmlStats) {
-      if(err) {
-        if(!err.code || err.code != 'ENOENT') {
-          return callback(err);
-        }
-      }
-      return callback(null, {
-        html: {
-          path: htmlPath,
-          stats: htmlStats
-        },
-        jade: {
-          path: jadePath,
-          stats: jadeStats
-        }
-      });
-    });
-  });
+    return false;
+  }
+  if (x === null || x === undefined || y === null || y === undefined) { return x === y; }
+  if (x === y || x.valueOf() === y.valueOf()) { return true; }
+
+  // if one of them is date, they must had equal valueOf
+  if (x instanceof Date) { return false; }
+  if (y instanceof Date) { return false; }
+
+  // if they are not function or strictly equal, they both need to be Objects
+  if (!(x instanceof Object)) { return false; }
+  if (!(y instanceof Object)) { return false; }
+
+  var p = Object.keys(x);
+  return Object.keys(y).every(function (i) { return p.indexOf(i) !== -1; }) ?
+    p.every(function (i) { return objectEquals(x[i], y[i]); }) : false;
 }
 
 function flatten(files, flattened) {
